d2cb6a1c2da6d8e905e820558a7a8a88739ced12
LyngGG <liuyang050828@gmail.com>
2026-05-12 19:21:09 +0200

    implementacion de redirecciones con dup2

--- main.c
+++ main.c
@@ -57,10 +57,46 @@ int main(void)
         int i;
         int prev_read = -1;
         int started = 0;
+        int in_fd = -1;
+        int out_fd = -1;
+        int err_fd = -1;
+        int redir_ok = 1;
         pid_t *pids = calloc(argvc, sizeof(pid_t));
 
         if (!pids) {
             perror("[ERROR] calloc");
+            continue;
+        }
+
+        if (filev[0]) {
+            in_fd = open(filev[0], O_RDONLY);
+            if (in_fd < 0) {
+                perror(filev[0]);
+                redir_ok = 0;
+            }
+        }
+
+        if (redir_ok && filev[1]) {
+            out_fd = open(filev[1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+            if (out_fd < 0) {
+                perror(filev[1]);
+                redir_ok = 0;
+            }
+        }
+
+        if (redir_ok && filev[2]) {
+            err_fd = open(filev[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+            if (err_fd < 0) {
+                perror(filev[2]);
+                redir_ok = 0;
+            }
+        }
+
+        if (!redir_ok) {
+            if (in_fd != -1) close(in_fd);
+            if (out_fd != -1) close(out_fd);
+            if (err_fd != -1) close(err_fd);
+            free(pids);
             continue;
         }
 
@@ -74,53 +110,29 @@ int main(void)
                 }
             }
 
-            pid_t pid = fork(); 
-            if (pid < 0) {
-                perror("[ERROR] fork");
-                /* Cerrar extremos y abortar */
-                if (pipefd[0] != -1) close(pipefd[0]);
-                if (pipefd[1] != -1) close(pipefd[1]);
-                break;
-            }
-
             if (pid == 0) {
                 if (prev_read != -1) {
                     dup2(prev_read, STDIN_FILENO);
-                } else if (filev[0]) {
-                    int fd = open(filev[0], O_RDONLY);
-                    if (fd < 0) {
-                        perror(filev[0]);
-                        exit(1);
-                    }
-                    dup2(fd, STDIN_FILENO);
-                    close(fd);
+                } else if (in_fd != -1) {
+                    dup2(in_fd, STDIN_FILENO);
                 }
 
                 if (i < argvc - 1) {
                     dup2(pipefd[1], STDOUT_FILENO);
-                } else if (filev[1]) {
-                    int fd = open(filev[1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
-                    if (fd < 0) {
-                        perror(filev[1]);
-                        exit(1);
-                    }
-                    dup2(fd, STDOUT_FILENO);
-                    close(fd);
+                } else if (out_fd != -1) {
+                    dup2(out_fd, STDOUT_FILENO);
                 }
 
-                if (filev[2] && i == argvc - 1) {
-                    int fd = open(filev[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
-                    if (fd < 0) {
-                        perror(filev[2]);
-                        exit(1);
-                    }
-                    dup2(fd, STDERR_FILENO);
-                    close(fd);
+                if (err_fd != -1 && i == argvc - 1) {
+                    dup2(err_fd, STDERR_FILENO);
                 }
 
                 if (prev_read != -1) close(prev_read);
                 if (pipefd[0] != -1) close(pipefd[0]);
                 if (pipefd[1] != -1) close(pipefd[1]);
+                if (in_fd != -1) close(in_fd);
+                if (out_fd != -1) close(out_fd);
+                if (err_fd != -1) close(err_fd);
 
                 execvp(argvv[i][0], argvv[i]);
                 perror(argvv[i][0]);
@@ -135,6 +147,9 @@ int main(void)
         }
 
         if (prev_read != -1) close(prev_read);
+        if (in_fd != -1) close(in_fd);
+        if (out_fd != -1) close(out_fd);
+        if (err_fd != -1) close(err_fd);
 
         if (!bg && started > 0) {
             waitpid(pids[started - 1], &status, 0);
